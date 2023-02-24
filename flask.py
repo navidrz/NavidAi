1a8ad09fa406870fc43cc097fe557e49ec5bb0ec
navidrz <124895301+navidrz@users.noreply.github.com>
2023-02-24 15:59:38 +0300

    Delete flask.py

--- flask.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-import gdown
-app = Flask(__name__)
-
-@app.route('/run-colab')
-def run_colab():
-    gdown.download('https://colab.research.google.com/drive/1HhwbSO7EKAN6l-6A8V3ZIFvk2Yi-JsQh', 'colab.ipynb', quiet=False)
-    return jsonify(message='colab notebook ran successfully')
